6a940dfc57cdf26134784d9288988d69ba24caae
Alberto Varela Sánchez <hello@albertovarela.net>
2023-12-16 18:23:11 +0100

    day15

--- src/day15/lens-library.test.ts
+++ src/day15/lens-library.test.ts
@@ -4,20 +4,16 @@ import part2 from "./part2";
 
 describe("Day 15: Lens Library", () => {
 	test("Part 1", async () => {
-		const input = `test
-input
-part1`;
+		const input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
 
 		const answer = part1(input);
-		expect(answer).toBe("outputpart1");
+		expect(answer).toBe("1320");
 	});
 
 	test("Part 2", async () => {
-		const input = `test
-input
-part2`;
+		const input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
 
 		const answer = part2(input);
-		expect(answer).toBe("outputpart2");
+		expect(answer).toBe("145");
 	});
 });
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,6 +7,24 @@ export type Matrix<T = string> = readonly T[][];
 
 export type Coordinate = readonly [number, number];
 
+type GrowToSize<T, N extends number, A extends T[]> = A["length"] extends N
+	? A
+	: GrowToSize<T, N, [...A, T]>;
+
+export type FixedArray<T, N extends number> = GrowToSize<T, N, []>;
+
+type Enumerate<
+	N extends number,
+	Acc extends number[] = [],
+> = Acc["length"] extends N
+	? Acc[number]
+	: Enumerate<N, [...Acc, Acc["length"]]>;
+
+export type Range<F extends number, T extends number> = Exclude<
+	Enumerate<T>,
+	Enumerate<F>
+>;
+
 export const lines = pipe(split("\n"), map(trim));
 
 export const toMatrix: (s: string) => Matrix = pipe(lines, map(split("")));
