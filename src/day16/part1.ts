ec41264fe979a1240302d39cb458a1befd8f9db0
Alberto Varela Sánchez <hello@albertovarela.net>
2023-12-16 22:19:13 +0100

    day 16 solutions

--- src/day16/part1.ts
+++ src/day16/part1.ts
@@ -1,19 +1,132 @@
-import { sum } from "../utils";
+import { Coordinate, Matrix, toMatrix } from "../utils";
 
-export default (input: string): string => {
-	return sum(input.replaceAll("\n", "").split(",").map(hash)).toString();
+type Direction = "up" | "down" | "right" | "left";
+
+type Beam = {
+	point: Coordinate;
+	direction: Direction;
 };
 
-function hash(input: string): number {
-	return input
-		.split("")
-		.map(getAscii)
-		.reduce((acc, value) => {
-			const result = ((acc + value) * 17) % 256;
-			return result;
-		}, 0);
+export default (input: string): string => {
+	const matrix = toMatrix(input);
+	const beams: Beam[] = [{ point: [0, 0], direction: "right" }];
+
+	const energized = new Set<string>(); // x,y
+	const energizedNodes = new Set<string>(); // x,y,direction
+
+	while (beams.length) {
+		const beam = beams[0];
+		if (!beam) {
+			break;
+		}
+
+		const [x, y] = beam.point;
+		const content = readCell(matrix, beam.point);
+		process(content, beam, beams, energizedNodes);
+		if (content) {
+			energized.add(`${x},${y}`);
+		}
+	}
+
+	return energized.size.toString();
+};
+
+function readCell(matrix: Matrix, [x, y]: Coordinate): string | undefined {
+	return matrix[y]?.[x];
 }
 
-function getAscii(input: string): number {
-	return input.charCodeAt(0);
+function process(
+	content: string | undefined,
+	beam: Beam,
+	beams: Beam[],
+	energizedNodes: Set<string>,
+) {
+	const removeCurrentBeam = () => beams.shift();
+
+	if (!content) {
+		removeCurrentBeam();
+		return;
+	}
+
+	if (content === ".") {
+		navigate(beam, beam.direction);
+		return;
+	}
+
+	const dirChangeId = `${beam.point[0]},${beam.point[1]},${beam.direction}`;
+	if (energizedNodes.has(dirChangeId)) {
+		removeCurrentBeam();
+		return;
+	}
+	energizedNodes.add(dirChangeId);
+
+	if (content === "/") {
+		navigate(
+			beam,
+			({ up: "right", down: "left", left: "down", right: "up" } as const)[
+				beam.direction
+			],
+		);
+		return;
+	}
+
+	if (content === "\\") {
+		navigate(
+			beam,
+			({ up: "left", down: "right", left: "up", right: "down" } as const)[
+				beam.direction
+			],
+		);
+		return;
+	}
+
+	if (content === "-") {
+		if (["right", "left"].includes(beam.direction)) {
+			navigate(beam, beam.direction);
+			return;
+		}
+		removeCurrentBeam();
+		beams.push({
+			direction: "left",
+			point: [beam.point[0] - 1, beam.point[1]],
+		});
+		beams.push({
+			direction: "right",
+			point: [beam.point[0] + 1, beam.point[1]],
+		});
+		return;
+	}
+
+	if (content === "|") {
+		if (["up", "down"].includes(beam.direction)) {
+			navigate(beam, beam.direction);
+			return;
+		}
+		beams.push({
+			direction: "up",
+			point: [beam.point[0], beam.point[1] - 1],
+		});
+		beams.push({
+			direction: "down",
+			point: [beam.point[0], beam.point[1] + 1],
+		});
+		removeCurrentBeam();
+		return;
+	}
+}
+
+function navigate(beam: Beam, direction: Direction) {
+	const {
+		point: [x, y],
+	} = beam;
+
+	const next = {
+		up: [x, y - 1],
+		down: [x, y + 1],
+		left: [x - 1, y],
+		right: [x + 1, y],
+	} as const;
+
+	beam.direction = direction;
+	beam.point = next[direction];
 }
--- src/day16/part2.ts
+++ src/day16/part2.ts
@@ -1,58 +1,162 @@
-import { FixedArray, Range, sum } from "../utils";
+import { Coordinate, Matrix, toMatrix } from "../utils";
 
-type Box = { label: string; length: number }[];
+type Direction = "up" | "down" | "right" | "left";
 
-export default (input: string): string => {
-	const boxes = Array.from({ length: 256 }, (_) => [] as Box) as FixedArray<
-		Box,
-		256
-	>;
-	const steps = input.replaceAll("\n", "").split(",");
-	for (const step of steps) {
-		if (step.includes("=")) {
-			const [label, lengthS] = step.split("=");
-			const length = parseInt(lengthS ?? "", 10);
-			const box = hash(label ?? "");
-			const index = boxes[box].findIndex((b) => b.label === label);
-			if (index !== -1) {
-				// biome-ignore lint/style/noNonNullAssertion: index will exist
-				boxes[box][index]!.length = length;
-				continue;
-			}
-
-			boxes[box].push({ label: label ?? "", length });
-			continue;
-		}
-
-		const label = step.slice(0, step.indexOf("-"));
-		const box = hash(label);
-		const index = boxes[box].findIndex((b) => b.label === label);
-		if (index >= 0) {
-			boxes[box] = boxes[box].filter((_, i) => i !== index);
-		}
-	}
-
-	return sum(boxes.map((box, index) => power(box, index + 1))).toString();
+type Beam = {
+	point: Coordinate;
+	direction: Direction;
 };
 
-function power(box: Box, boxNumber: number): number {
-	if (box.length === 0) {
-		return 0;
+export default (input: string): string => {
+	const matrix = toMatrix(input);
+	const results: number[] = [];
+
+	for (const [y, row] of matrix.entries()) {
+		results.push(
+			calculateEnergized(matrix, { point: [0, y], direction: "right" }),
+		);
+		results.push(
+			calculateEnergized(matrix, {
+				point: [row.length - 1, y],
+				direction: "left",
+			}),
+		);
 	}
 
-	return sum(box.map((lens, index) => lens.length * (index + 1) * boxNumber));
+	for (const [x] of matrix[0]?.entries() ?? []) {
+		results.push(
+			calculateEnergized(matrix, { point: [x, 0], direction: "down" }),
+		);
+		results.push(
+			calculateEnergized(matrix, {
+				point: [x, matrix.length - 1],
+				direction: "up",
+			}),
+		);
+	}
+
+	return Math.max(...results).toString();
+};
+
+function calculateEnergized(matrix: Matrix, startingBeam: Beam): number {
+	const beams: Beam[] = [startingBeam];
+
+	const energized = new Set<string>(); // x,y
+	const energizedNodes = new Set<string>(); // x,y,direction
+
+	while (beams.length) {
+		const beam = beams[0];
+		if (!beam) {
+			break;
+		}
+
+		const [x, y] = beam.point;
+		const content = readCell(matrix, beam.point);
+		process(content, beam, beams, energizedNodes);
+		if (content) {
+			energized.add(`${x},${y}`);
+		}
+	}
+
+	return energized.size;
 }
 
-function hash(input: string): Range<0, 255> {
-	return input
-		.split("")
-		.map(getAscii)
-		.reduce((acc, value) => {
-			const result = ((acc + value) * 17) % 256;
-			return result;
-		}, 0) as Range<0, 255>;
+function readCell(matrix: Matrix, [x, y]: Coordinate): string | undefined {
+	return matrix[y]?.[x];
 }
 
-function getAscii(input: string): number {
-	return input.charCodeAt(0);
+function process(
+	content: string | undefined,
+	beam: Beam,
+	beams: Beam[],
+	energizedNodes: Set<string>,
+) {
+	const removeCurrentBeam = () => beams.shift();
+
+	if (!content) {
+		removeCurrentBeam();
+		return;
+	}
+
+	if (content === ".") {
+		navigate(beam, beam.direction);
+		return;
+	}
+
+	const dirChangeId = `${beam.point[0]},${beam.point[1]},${beam.direction}`;
+	if (energizedNodes.has(dirChangeId)) {
+		removeCurrentBeam();
+		return;
+	}
+	energizedNodes.add(dirChangeId);
+
+	if (content === "/") {
+		navigate(
+			beam,
+			({ up: "right", down: "left", left: "down", right: "up" } as const)[
+				beam.direction
+			],
+		);
+		return;
+	}
+
+	if (content === "\\") {
+		navigate(
+			beam,
+			({ up: "left", down: "right", left: "up", right: "down" } as const)[
+				beam.direction
+			],
+		);
+		return;
+	}
+
+	if (content === "-") {
+		if (["right", "left"].includes(beam.direction)) {
+			navigate(beam, beam.direction);
+			return;
+		}
+		removeCurrentBeam();
+		beams.push({
+			direction: "left",
+			point: [beam.point[0] - 1, beam.point[1]],
+		});
+		beams.push({
+			direction: "right",
+			point: [beam.point[0] + 1, beam.point[1]],
+		});
+		return;
+	}
+
+	if (content === "|") {
+		if (["up", "down"].includes(beam.direction)) {
+			navigate(beam, beam.direction);
+			return;
+		}
+		beams.push({
+			direction: "up",
+			point: [beam.point[0], beam.point[1] - 1],
+		});
+		beams.push({
+			direction: "down",
+			point: [beam.point[0], beam.point[1] + 1],
+		});
+		removeCurrentBeam();
+		return;
+	}
+}
+
+function navigate(beam: Beam, direction: Direction) {
+	const {
+		point: [x, y],
+	} = beam;
+
+	const next = {
+		up: [x, y - 1],
+		down: [x, y + 1],
+		left: [x - 1, y],
+		right: [x + 1, y],
+	} as const;
+
+	beam.direction = direction;
+	beam.point = next[direction];
 }
