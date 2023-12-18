d04c52816c484956b7881b6c56b8d5374a127bd0
Alberto Varela Sánchez <hello@albertovarela.net>
2023-12-18 21:38:36 +0100

    solutions day 18

--- src/day18/part1.ts
+++ src/day18/part1.ts
@@ -1,3 +1,181 @@
+import { Coordinate, Direction, Matrix, lines, navigate } from "../utils";
+
+type Instruction = {
+	direction: Direction;
+	number: number;
+	color: string;
+};
+
+const toDirection: Record<string, Direction> = {
+	U: "up",
+	D: "down",
+	R: "right",
+	L: "left",
+} as const;
+
 export default (input: string): string => {
-	throw new Error("Not implemented");
+	const instructions = lines(input);
+	const data: Instruction[] = instructions
+		.map((instruction) => {
+			const [_, direction, number, color] =
+				instruction.match(/^(U|D|R|L)\s(\d+)\s\(\#(\w+)\)$/) ?? [];
+			return {
+				direction: toDirection[direction ?? ""],
+				number: Number(number),
+				color,
+			};
+		})
+		.filter(
+			(instruction): instruction is Instruction =>
+				!!instruction.direction && !!instruction.number && !!instruction.color,
+		);
+
+	const points = getPath(data);
+	const matrix = getMatrix(points);
+
+	const simplified = simplifyMatrix(matrix);
+	const innerTiles = simplified.map(countInnerTiles).reduce((a, b) => a + b, 0);
+
+	return `${innerTiles + points.length}`;
+};
+
+function getPath(data: Instruction[]) {
+	const startingPoint: Coordinate = [0, 0];
+	const points: Coordinate[] = [startingPoint];
+
+	for (const { direction, number } of data) {
+		for (let i = 0; i < number; i++) {
+			const lastPoint = points[points.length - 1] ?? startingPoint;
+			const newPoint = navigate(lastPoint, direction);
+			points.push(newPoint);
+		}
+	}
+
+	points.pop();
+
+	return points;
+}
+
+const getMatrix = (path: Coordinate[]): Matrix => {
+	//keep track of farthest coordinates
+	let top = 0;
+	let bottom = 0;
+	let left = 0;
+	let right = 0;
+
+	for (const [x, y] of path) {
+		if (x < left) left = x;
+		if (x > right) right = x;
+		if (y > bottom) bottom = y;
+		if (y < top) top = y;
+	}
+
+	const matrix: string[][] = [];
+
+	for (let y = top; y <= bottom; y++) {
+		for (let x = left; x <= right; x++) {
+			if (!matrix[y - top]) {
+				matrix[y - top] = [];
+			}
+
+			const index = path.findIndex(([px, py]) => px === x && py === y);
+
+			if (index !== -1) {
+				// biome-ignore lint/style/noNonNullAssertion: created above
+				matrix[y - top]![x - left] = getValue(path, index);
+			} else {
+				// biome-ignore lint/style/noNonNullAssertion: created above
+				matrix[y - top]![x - left] = " ";
+			}
+		}
+	}
+
+	return matrix;
+};
+
+function getValue(
+	path: Coordinate[],
+	index: number,
+): "-" | "|" | "7" | "J" | "L" | "F" {
+	const prev = index === 0 ? path[path.length - 1] : path[index - 1];
+	const current = path[index];
+	const post = path[index + 1] ?? path[0];
+	if (!post || !prev || !current) {
+		throw new Error("Invalid path");
+	}
+
+	const [prevX, prevY] = prev;
+	const [currentX, currentY] = current;
+	const [postX, postY] = post;
+
+	let direction: Direction | undefined = undefined;
+	if (prevX === currentX) {
+		direction = prevY > currentY ? "up" : "down";
+	} else if (prevY === currentY) {
+		direction = prevX > currentX ? "left" : "right";
+	} else {
+		throw new Error(`Invalid direction: ${prev} -> ${post}`);
+	}
+
+	if (direction === "up") {
+		if (postX === currentX) {
+			return "|";
+		}
+		return postX > currentX ? "F" : "7";
+	}
+
+	if (direction === "down") {
+		if (postX === currentX) {
+			return "|";
+		}
+		return postX > currentX ? "L" : "J";
+	}
+
+	if (direction === "right") {
+		if (postY === currentY) {
+			return "-";
+		}
+		return postY > currentY ? "7" : "J";
+	}
+
+	if (direction === "left") {
+		if (postY === currentY) {
+			return "-";
+		}
+		return postY > currentY ? "F" : "L";
+	}
+
+	throw new Error("Invalid direction");
+}
+
+const simplifyMatrix = (matrix: Matrix): Matrix => {
+	return matrix
+		.map((row) => {
+			return row
+				.join("")
+				.trim() // Remove outside tiles
+				.replaceAll("-", "") // make walls shorter
+				.replaceAll("FJ", "|") // same direction is a wall
+				.replaceAll("L7", "|") // same direction is a wall
+				.replaceAll("F7", "") // different direction is not wall
+				.replaceAll("LJ", "") // different direction is not wall
+				.split("");
+		})
+		.filter((row) => row.includes(" ")); // Remove rows without empty tiles
+};
+
+const countInnerTiles = (row: string[]): number => {
+	let count = 0;
+	let walls = 0;
+	for (let i = 0; i < row.length; i++) {
+		const cell = row[i];
+		if (cell === "|") {
+			walls++;
+		}
+		if (cell === " " && walls % 2 === 1) {
+			count++;
+		}
+	}
+
+	return count;
 };
--- src/day18/part2.ts
+++ src/day18/part2.ts
@@ -1,3 +1,104 @@
-export default (input: string): string => {
-	throw new Error("Not implemented");
+import { Coordinate, Direction, lines, navigate } from "../utils";
+
+type Instruction = {
+	direction: Direction;
+	number: number;
+	color: string;
 };
+
+type InstructionFixed = {
+	direction: Direction;
+	number: number;
+};
+
+const toDirection: Record<string, Direction> = {
+	U: "up",
+	D: "down",
+	R: "right",
+	L: "left",
+} as const;
+
+export default (input: string): string => {
+	const instructions = lines(input);
+	const data: InstructionFixed[] = instructions
+		.map((instruction) => {
+			const [_, direction, number, color] =
+				instruction.match(/^(U|D|R|L)\s(\d+)\s\(\#(\w+)\)$/) ?? [];
+			return {
+				direction: toDirection[direction ?? ""],
+				number: Number(number),
+				color,
+			};
+		})
+		.filter(
+			(instruction): instruction is Instruction =>
+				!!instruction.direction && !!instruction.number && !!instruction.color,
+		)
+		.map(fixInstruction);
+
+	const { points, perimeter } = getPathAndPerimeter(data);
+
+	return `${areaFromCoords(points) + perimeter / 2 + 1}`;
+};
+
+function getPathAndPerimeter(data: InstructionFixed[]) {
+	const startingPoint: Coordinate = [0, 0];
+	const points: Coordinate[] = [startingPoint];
+	let perimeter = 0;
+
+	for (const { direction, number } of data) {
+		const lastPoint = points[points.length - 1] ?? startingPoint;
+		const newPoint = navigate(lastPoint, direction, number);
+		points.push(newPoint);
+		perimeter += number;
+	}
+
+	points.pop();
+
+	return { points, perimeter };
+}
+
+// https://stackoverflow.com/questions/24793288/calculating-the-area-of-an-irregular-polygon-using-javascript
+function areaFromCoords(coordArray: Coordinate[]) {
+	const x = coordArray.reduce<number[]>((acc, value) => {
+		acc.push(value[0], value[1]);
+		return acc;
+	}, []);
+	let a = 0;
+
+	// Must have even number of elements
+	if (x.length % 2) {
+		throw new Error("coordArray must have an even number of elements");
+	}
+
+	// Process pairs, increment by 2 and stop at length - 2
+	for (let i = 0, iLen = x.length - 2; i < iLen; i += 2) {
+		a += (x[i] ?? 0) * (x[i + 3] ?? 0) - (x[i + 2] ?? 0) * (x[i + 1] ?? 0);
+	}
+
+	return Math.abs(a / 2);
+}
+
+function fixInstruction({ color }: Instruction): InstructionFixed {
+	const number = color.substring(0, 5);
+	const direction = color.substring(5);
+
+	const toDirection: Record<string, Direction> = {
+		0: "right",
+		1: "down",
+		2: "left",
+		3: "up",
+	} as const;
+
+	const realDirection = toDirection[direction];
+	if (!realDirection) {
+		throw new Error(`Invalid direction: ${direction}`);
+	}
+
+	const fixed: InstructionFixed = {
+		direction: realDirection,
+		number: parseInt(number, 16),
+	};
+
+	return fixed;
+}
